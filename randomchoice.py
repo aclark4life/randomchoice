200af79d66c6411cf5cb3aa71939d1f5c5e4d43c
Alex Clark <aclark@aclark.net>
2022-03-18 12:48:24 -0400

    Update

--- randomchoice.py
+++ randomchoice.py
@@ -1,7 +1,7 @@
 import random
 
-# https://stackoverflow.com/a/6494519/185820
-
 attendees = open("attendees.txt").read()
 attendees = attendees.split("\n")
+
+# https://stackoverflow.com/a/6494519/185820
 print(random.sample(attendees, 3))
