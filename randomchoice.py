45a8a8ade1db72c6333fb618d64ae3cbf4a2abf8
Alex Clark <aclark@aclark.net>
2022-03-18 12:50:57 -0400

    Update

--- randomchoice.py
+++ randomchoice.py
@@ -4,4 +4,6 @@ attendees = open("attendees.txt").read()
 attendees = attendees.split("\n")
 
 # https://stackoverflow.com/a/6494519/185820
-print(random.sample(attendees, 3))
+winners = random.sample(attendees, 3)
+
+print(winners)
