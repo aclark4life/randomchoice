97b77e7cd2b338d25d2cafaf86acf95888e3eadd
Alex Clark <aclark@aclark.net>
2022-03-19 11:42:03 -0400

    Update

--- randomchoice.py
+++ randomchoice.py
@@ -1,4 +1,9 @@
 import random
+import time
+from progress.bar import Bar
+
+
+bar = Bar("Processing", max=3)
 
 attendees = open("attendees3.txt").read()
 attendees = attendees.split("\n")
@@ -6,4 +11,11 @@ attendees = attendees.split("\n")
 # https://stackoverflow.com/a/6494519/185820
 winners = random.sample(attendees, 3)
 
-print(winners)
+# print(winners)
+
+for winner in winners:
+    time.sleep(3)
+    bar.next()
+    print("%s!\t\n" % winner)
+
+bar.finish
