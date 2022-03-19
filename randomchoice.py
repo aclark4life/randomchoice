bcc14b11176eaca47a78b7a88c0894d6fdb1eafb
Alex Clark <aclark@aclark.net>
2022-03-19 13:47:31 -0400

    Update

--- randomchoice.py
+++ randomchoice.py
@@ -6,6 +6,7 @@ attendees = open("attendees3.txt").read().split()
 # https://stackoverflow.com/a/6494519/185820
 winners = random.sample(attendees, 3)
 
+# Add suspense
 for winner in winners:
     print("Winner: %s!\n" % winner)
     time.sleep(2)
