700cb9aaeae71456a3963b7821290ba6eb14bb54
Alex Clark <aclark@aclark.net>
2022-12-31 10:31:36 -0500

    Update

--- randomchoice.py
+++ randomchoice.py
@@ -1,10 +1,10 @@
 import random
 import time
 
-attendees = open("attendees3.txt").read().split()
+attendees = open("attendees4.txt").read().split()
 
 # https://stackoverflow.com/a/6494519/185820
-winners = random.sample(attendees, 3)
+winners = random.sample(attendees, 1)
 
 # Add suspense
 for winner in winners:
